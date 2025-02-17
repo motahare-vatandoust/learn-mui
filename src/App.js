c624818b54808b751031177c4838bdce7c9713cd
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-17 22:14:08 +0330

    add some buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const BlueButton = styled(Button)({
+  backgroundColor: "skyblue",
+  color: "#888",
+  margin: "5px",
+  "&:hover": {
+    backgroundColor: "lightblue"
+  }
+});
+
 function App() {
   return (
     <div className="App">
-     Hello
+      <Button variant="text">Text</Button>
+      <Button startIcon={<SettingsIcon />} variant="contained">Contained</Button>
+      <BlueButton variant="contained">My unique button</BlueButton>
     </div>
   );
 }
