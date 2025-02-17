e2e70f2eead8d54fb886c9d96662516caa6bc778
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-17 22:29:03 +0330

    Add theme support

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { Button } from "@mui/material";
 import { styled } from "@mui/system";
 import SettingsIcon from '@mui/icons-material/Settings';
 
-const BlueButton = styled(Button)({
-  backgroundColor: "skyblue",
+const BlueButton = styled(Button)(({theme})=>({
+  backgroundColor: theme.palette.otherColor,
   color: "#888",
   margin: "5px",
   "&:hover": {
     backgroundColor: "lightblue"
   }
-});
+}));
 
 function App() {
   return (
     <div className="App">
       <Button variant="text">Text</Button>
-      <Button startIcon={<SettingsIcon />} variant="contained">Contained</Button>
+      <Button color="otherColor" startIcon={<SettingsIcon />} variant="contained">Contained</Button>
       <BlueButton variant="contained">My unique button</BlueButton>
     </div>
   );
