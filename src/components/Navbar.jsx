4cb90198ea632de6f1d654007bd5030a2e83551a
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-19 18:27:17 +0330

    Add dropdown menu to Navbar for user options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,13 @@ import {
   Badge,
   Box,
   InputBase,
+  Menu,
+  MenuItem,
   styled,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -26,21 +29,22 @@ const Icons = styled(Box)(({ theme }) => ({
   display: "none",
   alignItems: "center",
   gap: "20px",
-  [theme.breakpoints.up("sm")] : {
-    display: "flex"
-  }
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
 }));
 
 const UserBox = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   gap: "10px",
-  [theme.breakpoints.up("sm")] : {
-    display: "none"
-  }
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
 }));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -49,7 +53,7 @@ const Navbar = () => {
         </Typography>
         <Pets sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>
-          <InputBase placeholder="search" />
+          <InputBase placeholder="search..." />
         </Search>
         <Icons>
           <Badge badgeContent={4} color="error">
@@ -64,10 +68,11 @@ const Navbar = () => {
               height: 30,
             }}
             src="/public/random-girl.jpg"
+            onClick={(e) => setOpen(true)}
           />
         </Icons>
-        <UserBox>
-        <Avatar
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
             sx={{
               width: 30,
               height: 30,
@@ -77,6 +82,24 @@ const Navbar = () => {
           <Typography variant="span">John</Typography>
         </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false )}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
