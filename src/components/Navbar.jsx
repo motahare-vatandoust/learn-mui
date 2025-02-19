3197371d87ad495a45099bcfeeb6a34a23a440ae
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-19 14:42:02 +0330

    Add Navbar component with search and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,32 @@
+import { Pets } from "@mui/icons-material";
+import { AppBar, Box, InputBase, styled, Toolbar, Typography } from "@mui/material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%"
+}));
+const Icons = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
+}));
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Motahare Dev
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search><InputBase placeholder="search"/></Search>
+        <Icons>icons</Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
