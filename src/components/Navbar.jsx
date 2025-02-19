6fce1bd6e2ce5a0ec708279050953d65076b3197
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-19 15:04:48 +0330

    Enhance Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,45 @@
-import { Pets } from "@mui/icons-material";
-import { AppBar, Box, InputBase, styled, Toolbar, Typography } from "@mui/material";
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
 const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
   padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
-  width: "40%"
+  width: "40%",
 }));
+
 const Icons = styled(Box)(({ theme }) => ({
-  backgroundColor: "white",
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")] : {
+    display: "flex"
+  }
 }));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")] : {
+    display: "none"
+  }
+}));
+
 const Navbar = () => {
   return (
     <AppBar position="sticky">
@@ -22,8 +48,34 @@ const Navbar = () => {
           Motahare Dev
         </Typography>
         <Pets sx={{ display: { xs: "block", sm: "none" } }} />
-        <Search><InputBase placeholder="search"/></Search>
-        <Icons>icons</Icons>
+        <Search>
+          <InputBase placeholder="search" />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{
+              width: 30,
+              height: 30,
+            }}
+            src="/public/random-girl.jpg"
+          />
+        </Icons>
+        <UserBox>
+        <Avatar
+            sx={{
+              width: 30,
+              height: 30,
+            }}
+            src="/public/random-girl.jpg"
+          />
+          <Typography variant="span">John</Typography>
+        </UserBox>
       </StyledToolbar>
     </AppBar>
   );
