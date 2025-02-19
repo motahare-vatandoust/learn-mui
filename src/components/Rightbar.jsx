a7de441db34d45a63bcf1ece860e99ff093ecadd
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-19 22:11:54 +0330

    Complete LatestPhotos component layout and integrate it into Rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,19 +1,21 @@
 import { Avatar, AvatarGroup, Box, Typography } from "@mui/material";
+import LatestPhotos from "./LatestPhotos";
 
 const Rightbar = () => {
   return (
-    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }} width="200px">
       <Box position="fixed">
         <Typography variant="h6" fontWeight={100}>
           Online Friends
         </Typography>
         <AvatarGroup max={4}>
-  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-  <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-</AvatarGroup>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        </AvatarGroup>
+        <LatestPhotos />
       </Box>
     </Box>
   );
