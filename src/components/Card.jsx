44ef90db7442099e372112d70dc753fce177d1bd
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-19 21:52:46 +0330

    Enhance Card component with favorite checkbox

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,6 +14,8 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Checkbox } from "@mui/material";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -64,7 +66,7 @@ export default function RecipeReviewCard() {
       />
       <CardMedia
         component="img"
-        height="200"
+        height="500"
         image="/static/paella.jpg"
         alt="Paella dish"
       />
@@ -77,40 +79,12 @@ export default function RecipeReviewCard() {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+        <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color: "red"}}/>} />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
       </CardActions>
-      <CardContent>
-        <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-          set aside for 10 minutes.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-          over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-          stirring occasionally until lightly browned, 6 to 8 minutes. Transfer
-          shrimp to a large plate and set aside, leaving chicken and chorizo in
-          the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-          pepper, and cook, stirring often until thickened and fragrant, about
-          10 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth;
-          bring to a boil.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Add rice and stir very gently to distribute. Top with artichokes and
-          peppers, and cook without stirring, until most of the liquid is
-          absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-          shrimp and mussels, tucking them down into the rice, and cook again
-          without stirring, until mussels have opened and rice is just tender, 5
-          to 7 minutes more. (Discard any mussels that don&apos;t open.)
-        </Typography>
-        <Typography>
-          Set aside off of the heat to let rest for 10 minutes, and then serve.
-        </Typography>
-      </CardContent>
     </Card>
   );
 }
